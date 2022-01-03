862e167f9b60dfa9cffc3083196cbd78879e8423
Sven Sommerlade <y5523@softwareag.com>
2022-01-03 13:54:05 +0100

    updated vite config to fix url issue in gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: "./",
   server: {
     host: '0.0.0.0',
     hmr: {
