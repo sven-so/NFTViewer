18a0ca961c4ab7c2d4acc2375024aa1645bb84bf
Sven Sommerlade <y5523@softwareag.com>
2022-01-03 11:51:32 +0100

    Fix build for deployment

--- src/components/NFTRarity.tsx
+++ src/components/NFTRarity.tsx
@@ -15,7 +15,7 @@ export default function NFTRarity() {
   return (
     <section>
       <h1>{nft.name}</h1>
-      <div class={currentStyle}>
+      <div className={currentStyle}>
         <img src={nft.image} alt="" />
       </div>
       <p>{nft.description}</p>
